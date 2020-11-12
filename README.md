e0b3a84abbea54cb965346918365d20ce532a432
Ray Lee <kasenoshi@gmail.com>
2020-11-13 01:52:41 +0800

    Update to latest Instagram and support skiplist

--- README.md
+++ README.md
@@ -2,17 +2,22 @@
 unfollow all automatically through selenium
 * note that Instagram has a limit for unfollowing (15 in 15 minutes, no more than 1000 per day)
 
+* Tested on python 3.7.3
 * Tested on Mac 10.13.2 with Selenium 3.8.1, ChromeDriver 2.35
 * Tested on Instagram on Jan 20th, 2018
 
 ## Dependency
 * virtualenv
 * selenium
-* [ChromeDriver](https://sites.google.com/a/chromium.org/chromedriver/downloads)
+* [ChromeDriver](https://chromedriver.storage.googleapis.com/index.html)
+  * Download the ChromeDriver meet your chrome's version
+  * Include ChromeDriver on the path
+  * $ export PATH="$PATH:/path/to/chromedriver"
 
 ## Usage
-* virtualenv ENV
-* source ENV/bin/active
-* pip install selenium
-* python unfollow.py
+* $ python3 -m venv ENV
+* $ source ENV/bin/active
+* $ pip install selenium
+* edit config.json
+* $ python unfollow.py
 * enter your username and password
--- unfollow.py
+++ unfollow.py
@@ -1,5 +1,7 @@
 """This script unfollows all followings in entered Instagram account
 selenium is used to do the web manipulation"""
+import json
+import re
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,10 +13,15 @@ class InstagramUnfollower(object):
     """Class to unfollow all followings in Instagram"""
 
     def __init__(self):
-        print "Enter username:",
-        self.username = raw_input()
-        print "Enter password:",
-        self.password = raw_input()
+        print("Loading config.json ...")
+        with open('config.json') as json_file:
+            data = json.load(json_file)
+            self.username = data['username']
+            self.password = data['password']
+            self.skiplist = set()
+            for i in data['skiplist']:
+                self.skiplist.add(i)
+
         self.driver = webdriver.Chrome()
 
     def log_in(self):
@@ -25,8 +32,8 @@ class InstagramUnfollower(object):
         """
         driver = self.driver
 
-        log_in = driver.find_element_by_link_text('Log in')
-        log_in.click()
+        stop_condition = EC.presence_of_element_located((By.CSS_SELECTOR, 'button[type="submit"]'))
+        WebDriverWait(driver, 10).until(stop_condition)
 
         username = driver.find_element_by_css_selector('input[name="username"]')
         username.send_keys(self.username)
@@ -34,7 +41,7 @@ class InstagramUnfollower(object):
         password = driver.find_element_by_css_selector('input[name="password"]')
         password.send_keys(self.password)
 
-        button = driver.find_element_by_css_selector('button')
+        button = driver.find_element_by_css_selector('button[type="submit"]')
         button.click()
 
     def open_profile(self):
@@ -45,9 +52,10 @@ class InstagramUnfollower(object):
         """
         driver = self.driver
 
-        stop_condition = EC.presence_of_element_located((By.LINK_TEXT, 'Profile'))
-        profile = WebDriverWait(driver, 10).until(stop_condition)
-        profile.click()
+        stop_condition = EC.presence_of_element_located((By.CSS_SELECTOR, 'span.coreSpriteKeyhole'))
+        WebDriverWait(driver, 10).until(stop_condition)
+
+        driver.get('http://www.instagram.com/' + self.username)
 
     def unfollow_all(self):
         """Method to unfollow all followings on Instagram.
@@ -61,24 +69,41 @@ class InstagramUnfollower(object):
             while True:
                 stop_condition = EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, 'following'))
                 following = WebDriverWait(driver, 10).until(stop_condition)
+                print(following.text)
+                num_following = int(re.search(r'\d+', following.text).group())
                 following.click()
 
                 following_xpath = '//button[text()="Following"]'
+                unfollow_xpath = '//button[text()="Unfollow"]'
+
                 stop_condition = EC.presence_of_element_located((By.XPATH, following_xpath))
                 WebDriverWait(driver, 10).until(stop_condition)
                 followings = driver.find_elements(By.XPATH, following_xpath)
+                names = driver.find_elements(By.CSS_SELECTOR, 'a.notranslate')
+
+                i = 0
                 for following in followings:
+                    # skip for names in skiplist
+                    if names[i].text in self.skiplist:
+                        print("In skiplist, skip " + names[i].text)
+                        i += 1
+                        continue
+                    print("Unfollow " + names[i].text)
                     following.click()
+                    stop_condition = EC.presence_of_element_located((By.XPATH, unfollow_xpath))
+                    unfollow = WebDriverWait(driver, 10).until(stop_condition)
+                    unfollow.click()
                     # Instagram has limits for unfollowing
                     # (15 in 15 minutes, no more than 1000 per day)
                     time.sleep(60)
+                    i += 1
 
-                close = driver.find_element(By.XPATH, '//button[text()="Close"]')
-                close.click()
-
+                # Stop if check followings
+                if i == num_following:
+                    break
                 driver.refresh()
         except TimeoutException:
-            print "no followings left, program stops"
+            print("no followings left, program stops")
         finally:
             driver.quit()
 
